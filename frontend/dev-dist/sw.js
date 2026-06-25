34d19b1ab2293946b00a35be43b3f6e16e66a3e3
briaxcol-bit <briax.col@gmail.com>
2026-06-24 21:35:35 -0500

    chore: resolve dev-dist conflicts (generated files)

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-d488705a'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.7cbg5u14to"
+    "revision": "0.6uv72ce55mg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
